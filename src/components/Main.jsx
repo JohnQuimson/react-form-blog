001eff6ce4dfb895761ecf72ba88ba5c858015f2
JohnQuimson <john.quimson777@gmail.com>
2024-06-18 14:35:37 +0200

    feat: form

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,7 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Main = () => {
-  return <div>Main</div>;
+  const [article, setArticle] = useState([]);
+  const [articleTitle, setArticleTitle] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setArticle((array) => [...array, articleTitle]);
+    setArticleTitle('');
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Inserisci un articolo"
+          value={articleTitle}
+          onChange={(e) => setArticleTitle(e.target.value)}
+        />
+        <button>+</button>
+      </form>
+      <p>
+        <strong>Ingredienti:</strong>
+      </p>
+      <ul>
+        {article.map((article, index) => (
+          <li key={`article${index}`}>{article}</li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default Main;
